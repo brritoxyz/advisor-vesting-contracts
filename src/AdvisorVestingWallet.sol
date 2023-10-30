bafeb0d5fa431f63ba8588c2532f041d860c0282
kphed <kpcrypto69@gmail.com>
2023-10-30 10:03:13 -0400

    Add a method for withdrawing unvested tokens

--- src/AdvisorVestingWallet.sol
+++ src/AdvisorVestingWallet.sol
@@ -1,9 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import {Ownable} from "solady/auth/Ownable.sol";
 import {VestingWallet} from "openzeppelin-contracts/finance/VestingWallet.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 
-contract AdvisorVestingWallet is VestingWallet {
+contract AdvisorVestingWallet is Ownable, VestingWallet {
+    using SafeTransferLib for address;
+
+    address private constant _BRR = 0x6d80d90ce251985bF41A98c6FDd6b7b975Fff884;
+
+    // Protocol address (ownership may be transferred at will).
+    // Allows the protocol to withdraw the advisor tokens for ex-advisors.
+    address private constant _INITIAL_OWNER =
+        0x8Fcc36CCa8dE6E5d6c44d4de5F8fbCa86742e0af;
+
     // Advisor vesting starts on October 31, 2023 at 12:00pm EST.
     uint64 private constant _VESTING_START = 1698768000;
 
@@ -13,5 +24,17 @@ contract AdvisorVestingWallet is VestingWallet {
 
     constructor(
         address beneficiaryAddress
-    ) VestingWallet(beneficiaryAddress, _VESTING_START, _VESTING_DURATION) {}
+    ) VestingWallet(beneficiaryAddress, _VESTING_START, _VESTING_DURATION) {
+        _initializeOwner(_INITIAL_OWNER);
+    }
+
+    /**
+     * @notice Transfer BRR back to the protocol, minus the releasable amount.
+     */
+    function withdrawUnvested() external onlyOwner {
+        _BRR.safeTransfer(
+            owner(),
+            _BRR.balanceOf(address(this)) - releasable(_BRR)
+        );
+    }
 }
