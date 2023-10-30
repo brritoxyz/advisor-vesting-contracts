fe02db974d5832fb8988bfef8a54f7917e4475dc
kphed <kpcrypto69@gmail.com>
2023-10-30 10:18:06 -0400

    Add a contract comment header with advisor vesting details

--- src/AdvisorVestingWallet.sol
+++ src/AdvisorVestingWallet.sol
@@ -5,13 +5,22 @@ import {Ownable} from "solady/auth/Ownable.sol";
 import {VestingWallet} from "openzeppelin-contracts/finance/VestingWallet.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 
+/**
+ * @notice OpenZeppelin's VestingWallet with the following changes:
+ * - The project owner address can withdraw unvested BRR to end the relationship with an advisor.
+ * - ETH-related methods are overridden and do nothing, since advisors are compensated in BRR.
+ *
+ * For a list of our advisors and their vesting amounts:
+ * https://docs.google.com/spreadsheets/d/1HEKMYYa_LeYzG87Li8gEu_-cR9mY9pTpoqe8MIhvU68/edit#gid=1253912565.
+ */
 contract AdvisorVestingWallet is Ownable, VestingWallet {
     using SafeTransferLib for address;
 
-    address private constant _BRR = 0x6d80d90ce251985bF41A98c6FDd6b7b975Fff884;
+    // BRR token contract
+    address public constant TOKEN = 0x6d80d90ce251985bF41A98c6FDd6b7b975Fff884;
 
-    // Protocol address (ownership may be transferred at will).
-    // Allows the protocol to withdraw the advisor tokens for ex-advisors.
+    // Project address (ownership may be transferred at will).
+    // Allows the project owner to withdraw the advisor tokens for ex-advisors.
     address private constant _INITIAL_OWNER =
         0x8Fcc36CCa8dE6E5d6c44d4de5F8fbCa86742e0af;
 
@@ -32,9 +41,9 @@ contract AdvisorVestingWallet is Ownable, VestingWallet {
      * @notice Transfer BRR back to the protocol with the releasable amount deducted.
      */
     function withdrawUnvested() external onlyOwner {
-        _BRR.safeTransfer(
+        TOKEN.safeTransfer(
             owner(),
-            _BRR.balanceOf(address(this)) - releasable(_BRR)
+            TOKEN.balanceOf(address(this)) - releasable(TOKEN)
         );
     }
 
