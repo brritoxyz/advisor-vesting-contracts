e31975a91da3f07a02bd4781842345b1393b9971
kphed <kpcrypto69@gmail.com>
2023-12-01 13:53:00 -0500

    Update the deployment script to reflect advisor updates

--- script/AdvisorVestingWallet.s.sol
+++ script/AdvisorVestingWallet.s.sol
@@ -14,75 +14,23 @@ contract AdvisorVestingWalletScript is Script {
 
         // For a list of our advisors and their vesting amounts:
         // https://docs.google.com/spreadsheets/d/1HEKMYYa_LeYzG87Li8gEu_-cR9mY9pTpoqe8MIhvU68/edit#gid=1253912565.
-        address theLlamasTreasury = address(
-            new AdvisorVestingWallet(0x04467245D8d0BcF3a482af9e72fC4492C91f683E)
+        address joeySantoro = address(
+            new AdvisorVestingWallet(0x7E47dF2a371fA3488963E30bc914942eDcC48Fe5)
         );
-        address theLlamasCore = address(
-            new AdvisorVestingWallet(0x1C2D4fD89Fa7555945A85C690A601bF28275292A)
-        );
-        address vectorized = address(
-            new AdvisorVestingWallet(0x1F5D295778796a8b9f29600A585Ab73D452AcB1c)
-        );
+
+        // The two advisors below have had their allocations updated.
         address cogsy = address(
             new AdvisorVestingWallet(0x36bFF079a4532C2F818a65c6ad208cEA587417e7)
-        );
-        address kimboCharlie = address(
-            new AdvisorVestingWallet(0xd334533980C201c1b93e39bBE6bD1E04be5C0f5D)
-        );
-        address kimbo0xFelix = address(
-            new AdvisorVestingWallet(0x54F65A11Ac61dE39DC70379a3CA0FA268EEf952c)
-        );
-        address kimboIrate = address(
-            new AdvisorVestingWallet(0x8625AfdbF3744D433D850c681Ba881d028253c8A)
-        );
-        address kimboJustJousting = address(
-            new AdvisorVestingWallet(0x8561AeDA4Df9739980A7D09044DdF15694D00711)
         );
         address nap = address(
             new AdvisorVestingWallet(0x6531C0f565A451dB7d2409937230C4f153812953)
         );
-        address proteccLabsKris = address(
-            new AdvisorVestingWallet(0xb9D83D298D46C4dd73618F19a2A40084Ce36476a)
-        );
-        address proteccLabsMagnus = address(
-            new AdvisorVestingWallet(0x065CcE43ff745e6364669D46eF73ba6236f1A75c)
-        );
-        address feistyDogeDAO = address(
-            new AdvisorVestingWallet(0x1d4B9b250B1Bd41DAA35d94BF9204Ec1b0494eE3)
-        );
-        address pyk = address(
-            new AdvisorVestingWallet(0x17ae0a6BE2e97b384165626dB2569729d5006640)
-        );
-        address llamaAirforce = address(
-            new AdvisorVestingWallet(0x375B28A603Be144a646A506fB6673cc3182fC8Df)
-        );
-        address questDAO = address(
-            new AdvisorVestingWallet(0x71F594eC26BD9f84B55735136216e0F359f19EFc)
-        );
 
-        _BRR.mint(theLlamasTreasury, 20_000_000e18);
-        _BRR.mint(theLlamasCore, 10_000_000e18);
-        _BRR.mint(vectorized, 10_000_000e18);
-        _BRR.mint(cogsy, 2_500_000e18);
-        _BRR.mint(kimboCharlie, 625_000e18);
-        _BRR.mint(kimbo0xFelix, 625_000e18);
-        _BRR.mint(kimboIrate, 625_000e18);
-        _BRR.mint(kimboJustJousting, 625_000e18);
-        _BRR.mint(nap, 2_500_000e18);
-        _BRR.mint(proteccLabsKris, 500_000e18);
-        _BRR.mint(proteccLabsMagnus, 500_000e18);
-        _BRR.mint(feistyDogeDAO, 1_000_000e18);
-        _BRR.mint(pyk, 2_500_000e18);
-        _BRR.mint(llamaAirforce, 5_000_000e18);
-        _BRR.mint(questDAO, 1_000_000e18);
+        _BRR.transfer(joeySantoro, 2_500_000e18);
 
-        // The advisors below have not provided us with a new Gnosis Safe address on Base.
-        // The below will be updated as we receive their new addresses.
-        // address ribbonatiDAO = address(
-        //     new AdvisorVestingWallet(0xFb2CE50C4c8024E037e6be52dd658E2Be23d93Db)
-        // );
-
-        // _BRR.mint(ribbonatiDAO, 100_000e18);
+        // Each advisor below has already received a portion of their advisor tokens, the amounts below are the remainder.
+        _BRR.transfer(cogsy, 295_434e18);
+        _BRR.transfer(nap, 295_430e18);
 
         vm.stopBroadcast();
     }
--- script/interfaces/IBRR.sol
+++ script/interfaces/IBRR.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 
 interface IBRR {
     function mint(address to, uint256 amount) external;
+
+    function transfer(address to, uint256 amount) external;
 }
